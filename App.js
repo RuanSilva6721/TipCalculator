2a3ea673e5dcc0ea2b34cc1d8da9e8363c873d69
Ruan Felipe <ruansilva6721@gmail.com>
2023-06-20 17:14:42 -0300

    setup init

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, Buttom } from 'react-native';
+import { styled } from 'styled-components/native';
 
-export default function App() {
+
+const Page = styled.SafeAreaView`
+  flex:1;
+  align-items:center;
+`;
+const HeaderText = styled.Text`
+  font-size:25px;
+  margin-top:30px;
+
+  
+
+`;
+const Input = styled.TextInput`
+  width:90%;
+  height:50px;
+  font-size:18px;
+  background-color:#EEE;
+  margin-top:20px;
+  border-radius:10px;
+  padding:10px;
+
+`;
+
+export default () => {
   return (
-    <View>
-      <Text></Text>
-     </View>
+    <Page>
+      <HeaderText>Calculadora De Gorjeta</HeaderText>
+      <Input/>
+     </Page>
   );
 }
