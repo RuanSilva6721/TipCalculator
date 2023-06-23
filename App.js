aabd5ae2e3047593a89bca91f033f370cecf8a54
Ruan Felipe <ruansilva6721@gmail.com>
2023-06-23 15:54:34 -0300

    add function of tip

--- App.js
+++ App.js
@@ -24,12 +24,69 @@ const Input = styled.TextInput`
   padding:10px;
 
 `;
+const CalcButton = styled.Button`
+  margin-top:20px;
+
+`;
+const ResultArea = styled.View`
+  width:100%;
+  margin-top:30px;
+  background-color:#EEE;
+  padding:20px;
+  justify-content:center;
+  align-items:center;
+
+`;
+
+const ResultItemTitle = styled.Text`
+  font-size:18px;
+  font-weight:bold;
+`;
+const ResultItem = styled.Text`
+  font-size:18px;
+  margin-bottom:30px;
+`;
 
 export default () => {
+
+  const [bill, setBill] = useState('');
+  const [tip, setTip] = useState(0);
+
+  const calc= () =>{
+    let nBill=parseFloat(bill)
+
+    if(nBill){
+      setTip((10/100)* nBill);
+    }else{
+      alert("Digite o valor da conta");
+    }
+  }
+
+
   return (
     <Page>
       <HeaderText>Calculadora De Gorjeta</HeaderText>
-      <Input/>
+      <Input
+      placeholder="Quanto deu a conta?"
+      placeholderTextColor="#000"
+      keyboardType='numeric'
+      value={bill}
+      onChangeText={n=>setBill(n)}
+      />
+      <CalcButton title='Calcular' onPress={calc}/>
+      {tip>0&&
+      <ResultArea>
+        <ResultItemTitle>Valor da Conta</ResultItemTitle>
+        <ResultItem>R$ {parseFloat(bill).toFixed(2)}</ResultItem>
+
+        <ResultItemTitle>Valor da Gorjeta</ResultItemTitle>
+        <ResultItem>R$ {tip.toFixed(2)}</ResultItem>
+
+        <ResultItemTitle>Valor Total</ResultItemTitle>
+        <ResultItem>R$ {(parseFloat(bill)+ tip).toFixed(2)}</ResultItem>
+
+      </ResultArea>
+      }
      </Page>
   );
 }
